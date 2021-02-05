7315153981981d00c952a57b28febd4d0b059ea0
Samicap <lovestochasewindmills@gmail.com>
2021-02-05 22:53:54 +0000

    add Admin dashboard stats for product count and categories

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  # http_basic_authenticate_with :name => ENV["HTTP_USER"], :password => ENV["HTTP_PASSWORD"]
 
   def index
     @products = Product.all.order(created_at: :desc)
