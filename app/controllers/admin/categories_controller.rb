10d9cb8d489e762d9a9e2957847699e356507243
Samicap <lovestochasewindmills@gmail.com>
2021-02-06 01:04:35 +0000

    added all code for adding a new category as an admin

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,18 +3,18 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV['HTTP_USER'], password: ENV['HTTP_PASSWORD']
 
   def index
-    @product = Product.order(id: :desc).all
+    @categories = Category.order(id: :desc).all
   end
 
   def new
-    @product = Product.new
+    @category = Category.new
   end
 
   def create
-    @product = Product.new(product_params)
+    @category = Category.new(category_params)
 
-    if @product.save
-      redirect_to [:admin, :products], notice: 'Product created!'
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
     else
       render :new
     end
@@ -22,14 +22,9 @@ class Admin::CategoriesController < ApplicationController
 
   private
 
-  def product_params
-    params.require(:product).permit(
+  def category_params
+    params.require(:category).permit(
       :name,
-      :description,
-      :category_id,
-      :quantity,
-      :image,
-      :price
     )
   end
 end
