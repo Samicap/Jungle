6ace136a441285c2ec9027b1f144d42ed73f1c51
Samicap <lovestochasewindmills@gmail.com>
2021-02-06 17:10:46 +0000

    no change

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,7 +3,7 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV['HTTP_USER'], password: ENV['HTTP_PASSWORD']
 
   def index
-    @categories = Category.order(id: :desc).all
+    @categories = Category.order(:name)
   end
 
   def new
